ec5c90110110e79d61eb49a06bf2d8e935ca75cf
Joao Assuncao <jlassuncao2005@gmail.com>
2026-09-17 18:55:52 -0300

    feat: funcoes dos cards

--- aula1/app.js
+++ aula1/app.js
@@ -12,12 +12,34 @@ const btnQuero= document.querySelector("nav button:nth-child(4)")
 
 // Declaração do rodapé
 const rodape = document.querySelector("footer small");
+const lista = document.querySelector("#lista")
+const cards = lista.querySelectorAll(".card")
 
 btnTodos.textContent += ` (${TOTAL})`
 btnAssistido.textContent += ` (${ASSISTIDOS})`
 btnAssistindo.textContent += ` (${ASSISTINDO})`
 btnQuero.textContent += ` (${QUERO})`
-
 rodape.textContent += ` · ${TOTAL} filmes cadastrados`
 
+function rotuloStatus(status) {
+    if(status === "assistido") {return "Assistido"};
+    if(status === "assistindo") {return "Assistindo"};
+    if(status === "quero") {return "Quero Assistir"}
+    return status;
+}
 
+const estrelas = (nota) => {
+    let resultaddo = '';
+    for (let i=1; i <= 5; i++) {
+        resultaddo += i <= nota ? '★' : '☆';
+    }
+    return resultaddo;
+}
+
+for (const card of cards) {
+    const badge = card.querySelector(".badge");
+    const nota = card.querySelector(".nota")
+
+    badge.textContent = rotuloStatus('assistindo')
+    nota.textContent += estrelas(3)
+}
