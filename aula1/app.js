7f12d6cc0019890c7d30d44506fa9dd8266d3590
joaoassuncao-dev <jlassuncao2005@gmail.com>
2026-09-21 15:40:54 -0300

    feat: adicionando listeners

--- aula1/app.js
+++ aula1/app.js
@@ -15,7 +15,31 @@ const btnQuero= document.querySelector("nav button:nth-child(4)")
 // Declaração do rodapé
 const rodape = document.querySelector("footer small");
 const lista = document.querySelector("#lista")
-// const cards = lista.querySelectorAll(".card")
+
+// Declaracao de variaveis do modal (form-filme)
+let editandoId = null
+const modal = document.querySelector("#modal")
+const form = document.querySelector("#form-filme")
+const abrir = () => modal.hidden = false;
+const fechar = () => modal.hidden = true;
+
+document.querySelector("header button")
+    .addEventListener("click", (event) => {
+        editandoId = null;
+        form.reset();
+        abrir();
+    })
+
+document.addEventListener("keydown", (e) => {
+    if (e.key == "Escape" && !modal.hidden) {
+        fechar()
+    }
+})
+
+document.querySelector(".btnCancelar")
+    .addEventListener("click", () => {
+        fechar()
+    })
 
 btnTodos.textContent += ` (${TOTAL})`
 btnAssistido.textContent += ` (${ASSISTIDOS})`
@@ -38,29 +62,42 @@ const estrelas = (nota) => {
     return resultaddo;
 }
 
+// renderizador dos cards na tela, cresce conforme o .json cresce
 function renderCards(filmes) {
     const cards = filmes.map((f) => `
-        <article class="card" data-id="${f.id}">
-            <img src="${f.poster}"width="90">
+       <article class="card" data-id="${f.id}" tabindex="0" role="button" aria-expanded="false">
+          <!-- Conteúdo sempre visível -->
+            <img src="${f.poster}" alt="Poster de ${f.titulo}" width="90">
             <h2>${f.titulo}</h2>
             <p>${f.ano}</p>
-            <p class="nota">${estrelas(f.nota)}:</p>
-            <span class="badge">${rotuloStatus(f.status)}</span>
-            <p>${f.comentario}</p>
+            <p class="nota">${estrelas(f.nota)}</p>
+            <span class="badge" ${f.status}>${rotuloStatus(f.status)}</span>
             <div class="acoes">
                 <button>editar</button><button>remover</button>
             </div>
-        </article>`).join("");
-        lista.innerHTML = cards;
+        </article>
+      `).join("");
+    lista.innerHTML = cards;
+
 }
 
-// for (const card of cards) {
-//     // const badge = card.querySelector(".badge");
-//     // const nota = card.querySelector(".nota")
-
-//     // badge.textContent = rotuloStatus('assistindo')
-//     // nota.textContent += estrelas(3)
-// }
-
 renderCards(filmesInicias)
+
+function expandCards(card) {
+    const expandido = card.classList.toggle('expandido');
+    card.setAttribute('aria-expanded', expandido);
+}
+
+lista.addEventListener('click', (event) => {
+    if (event.target.closest('button')) {
+        return;
+    }
+
+    const card = event.target.closest('.card')
+    if (card) {
+        expandCards(card)
+    }
+
+})
+
 
